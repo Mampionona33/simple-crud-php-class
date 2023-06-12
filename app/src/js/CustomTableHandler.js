ce6a92edf122a2034c70753f5e9c6986b8ca1a55
Mampionona33 <ramamps33@gmail.com>
2023-06-12 21:55:20 +0300

    create modal content for edit user

--- app/src/js/CustomTableHandler.js
+++ app/src/js/CustomTableHandler.js
@@ -47,10 +47,62 @@ function generateModalContent(title, rowId, userId) {
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <!-- Contenu du formulaire d'édition -->
-                <form>
-                    <label>Test</label>
-                </form>
+              <!-- Contenu du formulaire d'édition -->
+                <div class="d-flex justify-content-center align-items-center"">
+                  <div class="col-9">
+                        <div class="form-group row">
+                            <label for="name" class="col-sm-3 col-form-label">Nom</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control form-control-sm" name="name" id="name" value="$name" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="lastName" class="col-sm-3 col-form-label">Prénoms</label>
+                            <div class="col-sm-9">
+                                <input type="text" name="lastName" id="lastName" value="$lastName" required class="form-control form-control-sm">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="birthday" class="col-sm-3 col-form-label">Date de naissance</label>
+                            <div class="col-sm-9">
+                                <input type="date" name="birthday" id="birthday" value="$birthday" required class="form-control form-control-sm">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-3 col-form-label">Email</label>
+                            <div class="col-sm-9">
+                                <input type="email" name="email" id="email" value="$email" class="form-control form-control-sm">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="address" class="col-sm-3 col-form-label">Adresse</label>
+                            <div class="col-sm-9">
+                                <input type="text" name="address" id="address" value="$address" class="form-control form-control-sm">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="tel" class="col-sm-3 col-form-label">Tel</label>
+                            <div class="col-sm-9">
+                                <input type="tel" name="tel" id="tel" value="$tel" class="form-control form-control-sm">
+                            </div>
+                        </div>
+                        <fieldset>
+                            <p class="form-label col-form-label">Sélectionnez la civilité</p>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" id="Mr" name="civilite" value="Mr" $civiliteMrChecked>
+                                <label class="form-check-label" for="Mr">Mr</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" id="Mme" name="civilite" value="Mme" $civiliteMmeChecked>
+                                <label class="form-check-label" for="Mme">Mme</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" id="Mlle" name="civilite" value="Mlle" $civiliteMlleChecked>
+                                <label class="form-check-label" for="Mlle">Mlle</label>
+                            </div>
+                        </fieldset>
+                  </div>
+              </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
