13558028bd28c7136f80a636105e4b1397cfb6d3
Mampionona33 <ramamps33@gmail.com>
2023-06-14 06:10:59 +0000

    add searchbar for voters table

--- app/views/TableVoters.class.php
+++ app/views/TableVoters.class.php
@@ -2,17 +2,24 @@
 final class TableVoters
 {
     private $btnDetails = false;
+    private $searchBar = true;
     public function renderTable(array $data): string
     {
         $tableHeaders = ["id_voter", "Nom", "Prenoms", "date de naissance", "adresse", "civilité"];
         $tableVisitor = new CustomTable($tableHeaders, $data);
         $this->btnDetails && $tableVisitor->setBtnDatailState($this->btnDetails);
+        $this->searchBar && $tableVisitor->setSearchBarVisible($this->searchBar);
         return $tableVisitor->renderTable();
     }
 
     function setBtnDetailsVisible(bool $btnDetailsState): void
     {
         $this->btnDetails = $btnDetailsState;
+    }
+
+    function setSearchBarVisible(bool $searchBarState): void
+    {
+        $this->searchBar = $searchBarState;
     }
 
 }
